561c8cffdebe4906bf2e541e8708db5ffbeb7476
excy <dan.protsenko@gmail.com>
2024-03-25 21:51:19 +0300

    refactor main

---
+++ includes.h
@@ -0,0 +1,17 @@
+#ifndef ABOBAVULKAN_INCLUDES_H
+#define ABOBAVULKAN_INCLUDES_H
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan_beta.h>
+
+#include "state.cpp"
+
+#endif //ABOBAVULKAN_INCLUDES_H
---
+++ state.cpp
@@ -0,0 +1,21 @@
+#include "includes.h"
+
+static bool show_demo_window = true;
+static bool show_another_window = false;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+static ImGuiIO io;
+
+// GFX
+static VkAllocationCallbacks *g_Allocator = nullptr;
+static VkInstance g_Instance = VK_NULL_HANDLE;
+static VkPhysicalDevice g_PhysicalDevice = VK_NULL_HANDLE;
+static VkDevice g_Device = VK_NULL_HANDLE;
+static uint32_t g_QueueFamily = (uint32_t) -1;
+static VkQueue g_Queue = VK_NULL_HANDLE;
+static VkDebugReportCallbackEXT g_DebugReport = VK_NULL_HANDLE;
+static VkPipelineCache g_PipelineCache = VK_NULL_HANDLE;
+static VkDescriptorPool g_DescriptorPool = VK_NULL_HANDLE;
+
+static ImGui_ImplVulkanH_Window g_MainWindowData;
+static int g_MinImageCount = 2;
+static bool g_SwapChainRebuild = false;
